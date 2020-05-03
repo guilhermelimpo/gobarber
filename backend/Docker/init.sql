37978e38137ce403cb794a7eb7308559a942f675
Guilherme Limpo <guilhermelimpo@gmail.com>
2020-05-02 22:13:57 -0300

    Login e cadastro de usuário

---
+++ backend/Docker/init.sql
@@ -0,0 +1,5 @@
+-- CREATE USER docker WITH PASSWORD 'docker';
+
+CREATE DATABASE gostack_gobarber;
+-- GRANT ALL PRIVILEGES ON DATABASE gostack_gobarber TO docker;
+-- GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO docker;
